9a459c0df91dbec513454dbdd67d3e115acb6668
Mohit Bhatt <bhattmohit869@gmail.com>
2023-07-07 09:10:17 +0530

    added modal sheets

--- src/NewUser/NewUserForm.js
+++ src/NewUser/NewUserForm.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import './NewUserForm.css';
+import Button from "../UI/Button";
+import ErrorModalSheet from "../UI/ErrorModalSheet";
 
 const NewUserForm = (props) => {
   const [inputName, setInputName] = useState("");
   const [inputAge, setInputAge] = useState("");
-
+  const [error , setError] = useState({});
+ 
   const nameInputHandler = (event) => {
     setInputName(event.target.value);
   };
+
   const ageInputHandler = (event) => {
     setInputAge(event.target.value);
   };
+
   const submitHandler = (event) => {
     event.preventDefault();
+    // if(inputName.length === 0 && inputAge.length ===0){
+    //   console.log("empty");
+    //   return;
+    // }
+    // if(inputName.length === 0){
+    //   console.log("name is empty");
+    //   return;
+    // }
+    // if(inputAge.length ===0 ){
+    //   console.log("age is empty");
+    //   return;
+    // }
+    
     const details = {
       name: inputName,
       age: inputAge,
@@ -22,7 +40,11 @@ const NewUserForm = (props) => {
     setInputAge("");
     setInputName("");
   };
+
+  
   return (
+    <div>
+  
     <form onSubmit={submitHandler}>
       <div className="form-controls">
         <div className="form-control">
@@ -31,12 +53,13 @@ const NewUserForm = (props) => {
         </div>
         <div className="form-control">
           <label>Age(in years)</label>
-          <input type="text" onChange={ageInputHandler} value={inputAge}/>
+          <input type="number" onChange={ageInputHandler} value={inputAge}/>
         </div>
       </div>
       
-      <button type="submit" className="submit-button">ADD DETAILS</button>
+      <Button onClick = {submitHandler} buttonName = {"ADD DETAILS"}/>
     </form>
+    </div>
   );
 };
 
--- src/UserData/UserItem.js
+++ src/UserData/UserItem.js
@@ -8,6 +8,21 @@ const UserItem = (props)=>{
     props.onDelete(props.id);
   }
 
+//   if(props.userName.length === 0 && props.userAge.length === 0)
+//   {
+//     console.log("empty")
+//     return;
+//   }
+//   if(props.userName.length=== 0){
+//     console.log("name cannot be empty");
+//     return ;
+//   }
+//   if(props.userAge.length === 0){
+//     console.log("age cannot be empty");
+//     return;
+
+// }
+  
   const details = props.userName + '(' +props.userAge +'years old )';
   return (
     <div className="user-item" onClick= {deleteHandler}>
